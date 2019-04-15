bc43983ed8fd174447aeb4f175a0c44debfdc863
shantelvivar <shantelvivar@gmail.com>
2019-04-15 13:27:40 -0500

    idk

--- src/sample/Information.java
+++
@@ -1,8 +0,0 @@
-package sample;
-
-public class Information {
-    public static String Name() {return ("CoderKids");}
-    public static final String Username = "sa";
-    public static final String Password = "123";
-
-}
